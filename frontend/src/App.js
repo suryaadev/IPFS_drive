251716d87cdc56fe70b79b680332901eed30842d
suryaadev <infiniterohitsurya@gmail.com>
2023-01-15 01:54:10 +0530

    file upload completed]

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react"
 import { ethers } from "ethers"
 import { conAddress, abi } from "./constants/index"
 import FileUpload from "./components/FileUpload"
+import Modal from "./components/Modal"
 
 function App() {
   const [state, setState] = useState({
@@ -12,10 +13,19 @@ function App() {
   })
 
   const [account, setAccount] = useState("")
+  const [modalOpen, setModalOpen] = useState(false)
 
   const connectWallet = async () => {
     const { ethereum } = window
     if (ethereum) {
+      window.ethereum.on("chianChanged", () => {
+        window.location.reload()
+      })
+
+      window.ethereum.on("accountsChanged", () => {
+        window.location.reload()
+      })
+
       const account = await ethereum.request({ method: "eth_requestAccounts" })
       const contractAddress = conAddress
       const contractABI = abi
@@ -24,18 +34,29 @@ function App() {
       const signer = provider.getSigner()
       const contract = new ethers.Contract(contractAddress, contractABI, signer)
       setState({ provider, signer, contract })
+    } else {
+      alert("Please connect to metamask")
     }
   }
 
   useEffect(() => {
     connectWallet()
   }, [])
-  
 
   console.log(state)
-  return <div className="App">
-    <h1>{account}</h1>
-  </div>
+  return (
+    <div className="App">
+      <h1 style={{ color: "white" }}>IPFS Drive</h1>
+      <div className="bg"></div>
+      <div className="bg bg2"></div>
+      <div className="bg bg3"></div>
+      <p style={{ color: "white" }}>
+        Account : {account ? account : "Please connect metamask"}
+      </p>
+      <FileUpload account={account} state={state} />
+      <Modal />
+    </div>
+  )
 }
 
 export default App
--- frontend/src/components/FileUpload.js
+++ frontend/src/components/FileUpload.js
@@ -1,5 +1,73 @@
-const FileUpload = () => {
-  return <div></div>;
-};
+import { useState, useEffect } from "react"
+import axios from "axios"
+import "./FileUpload.css"
 
-export default FileUpload;
+const FileUpload = ({ account, state }) => {
+  const [file, setFile] = useState(null)
+  const [fileName, setFileName] = useState("No Image Selected")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (file) {
+      try {
+        const formData = new FormData()
+        formData.append("file", file)
+
+        const resFile = await axios({
+          method: "post",
+          url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+          data: formData,
+          headers: {
+            pinata_api_key: `44f3732ce42f24350a30`,
+            pinata_secret_api_key: `fadbd65e5bc9b24b0fbdb255ccc9363ac5f0365eb65399e7f57e2d9276c02bf3`,
+            "Content-Type": "multipart/form-data",
+          },
+        })
+
+        const imgHash = `ipfs://${resFile.data.IpfsHash}`
+
+        await state.contract.add(account, imgHash)
+        alert("Image uploaded sucessfully !!!")
+        setFileName("No Image selected")
+        setFile(null)
+      } catch (error) {
+        alert("Unable to upload file to pinata")
+        console.error(error)
+      }
+    }
+  }
+
+  const retriveFile = (e) => {
+    const data = e.target.files[0]
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(data)
+    reader.onloadend = () => {
+      setFile(data)
+    }
+    setFileName(data.name)
+    e.preventDefault()
+  }
+
+  return (
+    <div className="top">
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="file-upload" className="choose">
+          Choose Image
+        </label>
+        <input
+          disabled={!account}
+          type="file"
+          id="file-upload"
+          name="data"
+          onChange={retriveFile}
+        />
+        <span className="textArea"> Image : {fileName}</span>
+        <button type="submit" className="upload">
+          Upload File
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default FileUpload
